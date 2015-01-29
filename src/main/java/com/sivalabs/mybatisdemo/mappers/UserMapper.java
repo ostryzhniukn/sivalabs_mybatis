31a478eb29cb0b8b26d10ffe16958509b9aff68a
ostryzhniukn <nazarostryzhniuk@gmail.com>
2015-01-29 12:08:42 +0200

    part 1 of tutorial completed

---
+++ src/main/java/com/sivalabs/mybatisdemo/mappers/UserMapper.java
@@ -0,0 +1,21 @@
+package com.sivalabs.mybatisdemo.mappers;
+
+import com.sivalabs.mybatisdemo.domain.User;
+
+import java.util.List;
+
+public interface UserMapper
+{
+
+    public void insertUser(User user);
+
+    public User getUserById(Integer userId);
+
+    public List<User> getAllUsers();
+
+    public void updateUser(User user);
+
+    public void deleteUser(Integer userId);
+
+}
+
